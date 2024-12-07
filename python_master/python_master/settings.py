32017b2e10c3af17abd0849288aaf9773dde4ebd
Jacek Ostański <jacek@ivolution.pl>
2024-12-07 15:15:24 +0100

    home page works

--- python_master/python_master/settings.py
+++ python_master/python_master/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "core_theme/static",
+    BASE_DIR / "core_theme/static/css/dist",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
