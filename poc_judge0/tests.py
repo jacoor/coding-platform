46855d201083105fdd73780c9fc2a93093ab180e
Jacek Ostański <jacek@ivolution.pl>
2024-12-07 13:54:34 +0100

    ruff formatting

--- poc_judge0/tests.py
+++ poc_judge0/tests.py
@@ -1,14 +1,15 @@
 import unittest
 
+
 # Test class
 class TestMathFunctions(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5) # noqa F821 (undefined name 'add')
-        self.assertEqual(add(-1, 1), 0) # noqa F821 (undefined name 'add')
-        self.assertEqual(add(-1, -1), -2) # noqa F821 (undefined name 'add')
-        self.assertEqual(add(-1, -1), -3) # noqa F821 (undefined name 'add')
+        self.assertEqual(add(2, 3), 5)  # noqa F821 (undefined name 'add')
+        self.assertEqual(add(-1, 1), 0)  # noqa F821 (undefined name 'add')
+        self.assertEqual(add(-1, -1), -2)  # noqa F821 (undefined name 'add')
+        self.assertEqual(add(-1, -1), -3)  # noqa F821 (undefined name 'add')
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2) # noqa F821 (undefined name 'subtract')
-        self.assertEqual(subtract(1, 1), 0) # noqa F821 (undefined name 'subtract')
-        self.assertEqual(subtract(-1, -1), 0) # noqa F821 (undefined name 'subtract')
+        self.assertEqual(subtract(5, 3), 2)  # noqa F821 (undefined name 'subtract')
+        self.assertEqual(subtract(1, 1), 0)  # noqa F821 (undefined name 'subtract')
+        self.assertEqual(subtract(-1, -1), 0)  # noqa F821 (undefined name 'subtract')
