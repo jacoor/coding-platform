a2d69b6ffd2bde1eba53c3a8048793a2f92ed972
Jacek Ostański <jacek@ivolution.pl>
2024-12-14 15:54:17 +0100

    ruff

--- python_master/core/tests/test_educational_path_page.py
+++ python_master/core/tests/test_educational_path_page.py
@@ -6,24 +6,25 @@ from core.models import EducationPath
 
 
 @pytest.fixture()
-def client():
+def client() -> Client:
     return Client()
+
 
 @pytest.fixture()
 def create_education_paths():
-    paths = [
+    return [
         EducationPath.objects.create(
             name=f"Path {i}",
             description=f"Description for path {i}",
             ordering=i,
-            difficulty="Easy" if i % 3 == 0 else "Medium" if i % 3 == 1 else "Hard"
+            difficulty="Easy" if i % 3 == 0 else "Medium" if i % 3 == 1 else "Hard",
         )
         for i in range(31)
     ]
-    return paths
+
 
 @pytest.mark.django_db()
-def test_education_path_list_first_page(client, create_education_paths):
+def test_education_path_list_first_page(client: Client, create_education_paths: list[EducationPath]) -> None:
     # Given
     url = reverse("education_path_list")
 
@@ -39,9 +40,10 @@ def test_education_path_list_first_page(client, create_education_paths):
         assert hasattr(path, "description")
         assert hasattr(path, "difficulty")
 
+
 @pytest.mark.django_db()
-@pytest.mark.parametrize("page, expected_count", [(2, 10), (3, 10), (4, 1)])
-def test_education_path_list_pagination(client, create_education_paths, page, expected_count):
+@pytest.mark.parametrize(("page", "expected_count"), [(2, 10), (3, 10), (4, 1)])
+def test_education_path_list_pagination(client: Client, create_education_paths: list[EducationPath], page: int, expected_count: int) -> None:
     # Given
     url = reverse("education_path_list")
 
@@ -53,8 +55,9 @@ def test_education_path_list_pagination(client, create_education_paths, page, ex
     assert "paths" in response.context
     assert len(response.context["paths"]) == expected_count  # Check if the correct number of paths are displayed per page
 
+
 @pytest.mark.django_db()
-def test_education_path_list_empty(client):
+def test_education_path_list_empty(client: Client) -> None:
     # Given
     url = reverse("education_path_list")
 
