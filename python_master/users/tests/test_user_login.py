3e1767fa937df785feafa2148852a6cc1e386219
Jacek Ostański <jacek@ivolution.pl>
2024-12-14 14:36:52 +0100

    fixed tests

--- python_master/users/tests/test_user_login.py
+++ python_master/users/tests/test_user_login.py
@@ -1,14 +1,18 @@
+import os
+
 import pytest
 from django.contrib.auth import get_user_model
 from django.test import Client
 from django.urls import reverse
 
 User = get_user_model()
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "adminpassword")  # nosec
 
 
 @pytest.fixture()
-def admin_user(db) -> User:
-    return User.objects.create_superuser(email="admin@example.com", password="adminpassword")  # nosec
+def admin_user(db: any) -> User:
+    password = ADMIN_PASSWORD
+    return User.objects.create_superuser(email="admin@example.com", password=password)  # nosec
 
 
 @pytest.fixture()
@@ -18,12 +22,12 @@ def client() -> Client:
 
 def test_admin_login(client: Client, admin_user: User) -> None:
     login_url = reverse("admin:login")
-    response = client.post(login_url, {"username": admin_user.email, "password": "adminpassword"})
+    response = client.post(login_url, {"username": admin_user.email, "password": ADMIN_PASSWORD})
     assert response.status_code == 302  # Redirect to admin index page
     assert response.url == reverse("admin:index") or response.url == "/accounts/profile/"
 
 
-def test_invalid_login(db, client: Client) -> None:
+def test_invalid_login(db: any, client: Client) -> None:
     login_url = reverse("admin:login")
     response = client.post(login_url, {"username": "invalid@example.com", "password": "wrongpassword"})
     assert response.status_code == 200  # Login page is re-rendered
