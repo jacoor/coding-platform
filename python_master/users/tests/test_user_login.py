8ef8bc52a5174a4fdd5b9a35b37fb329468b2d43
Jacek Ostański <jacek@ivolution.pl>
2024-12-14 14:30:08 +0100

    ruff

--- python_master/users/tests/test_user_login.py
+++ python_master/users/tests/test_user_login.py
@@ -7,20 +7,23 @@ User = get_user_model()
 
 
 @pytest.fixture()
-def admin_user(db):
-    return User.objects.create_superuser(email="admin@example.com", password="adminpassword")
+def admin_user(db) -> User:
+    return User.objects.create_superuser(email="admin@example.com", password="adminpassword")  # nosec
+
 
 @pytest.fixture()
-def client():
+def client() -> Client:
     return Client()
 
-def test_admin_login(client, admin_user):
+
+def test_admin_login(client: Client, admin_user: User) -> None:
     login_url = reverse("admin:login")
     response = client.post(login_url, {"username": admin_user.email, "password": "adminpassword"})
     assert response.status_code == 302  # Redirect to admin index page
     assert response.url == reverse("admin:index") or response.url == "/accounts/profile/"
 
-def test_invalid_login(db, client):
+
+def test_invalid_login(db, client: Client) -> None:
     login_url = reverse("admin:login")
     response = client.post(login_url, {"username": "invalid@example.com", "password": "wrongpassword"})
     assert response.status_code == 200  # Login page is re-rendered
