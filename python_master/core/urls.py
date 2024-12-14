a57b110db4c308a381f3031c8f41f16d2ce7f5ac
Jacek Ostański <jacek@ivolution.pl>
2024-12-14 15:52:36 +0100

    some tests, but not great

--- python_master/core/urls.py
+++ python_master/core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, SubmitView
-from .views import EducationPathListView
+from .views import EducationPathListView, IndexView, SubmitView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
